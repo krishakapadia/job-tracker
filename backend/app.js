4594206e4911e333b42f86e45fdf990f343e919f
Krisha Kapadia <krisha.kapadia@somaiya.edu>
2025-09-24 01:52:46 +0530

    Initial backend setup

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
